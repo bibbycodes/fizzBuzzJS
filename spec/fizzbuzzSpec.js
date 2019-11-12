3324d258e6f4f08edb1570ca8095b8b668df609c
Robert <bibbycodes@gmail.com>
2019-11-12 14:30:56 +0000

    tidies up code

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -3,35 +3,28 @@ describe("JavaBuzz",function() {
 
   beforeEach(function(){
     test = new FizzBuzz
-
   })
   it("returns fizz when 3 is passed in", function() {
-
     expect(test.play(3)).toEqual("Fizz");
   })
 
   it("returns Buzz when 5 is passed in", function() {
-
     expect(test.play(5)).toEqual("Buzz");
   })
 
   it("returns fizz when a multiple of 3 is passed in", function() {
-
     expect(test.play(9)).toEqual("Fizz");
   })
 
   it("returns buzz when a multiple of 5 is passed in", function() {
-
     expect(test.play(10)).toEqual("Buzz");
   })
 
   it("returns fizzbuzz when a multiple of 3 and 5 is passed in", function() {
-
     expect(test.play(15)).toEqual("FizzBuzz");
   })
 
   it("returns input when not a multiple of 3 or 5", function() {
-
     expect(test.play(16)).toEqual(16);
   })
 
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,19 +1,13 @@
 class FizzBuzz {
-
-
   play(number){
-
-
     if (number % 15 == 0) {
-      return "FizzBuzz"
-  }
-    else if (number % 3 == 0) {
-      return "Fizz"
-  } else if (number % 5 == 0) {
-      return "Buzz"
-    }
-    else {
+        return "FizzBuzz"
+    } else if (number % 3 == 0) {
+        return "Fizz"
+    } else if (number % 5 == 0) {
+        return "Buzz"
+    } else {
         return number
-      }
+    }
   }
 }
